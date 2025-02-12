0fee9a4f60579e9681cfca5e47bfe5877b654cc4
DadBytes <bpmckim@proton.me>
2025-02-12 08:24:54 -0500

    add ability to move left and right

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 from constants import *
-import player
+from player import Player
 
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
 
+    clock = pygame.time.Clock()
     dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -17,12 +18,12 @@ def main():
                 return
 
         screen.fill("black")
-        main_player = player.Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-        main_player.draw(screen)
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
 
-        elap_time = clock.tick(60)
-        dt = elap_time / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
