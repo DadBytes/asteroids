f1270f5fce5e449d85ac1bdbe0cd002bf8512613
DadBytes <bpmckim@icloud.com>
2025-12-15 16:24:13 -0500

    Use basic sprite object

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 from pygame.time import Clock
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
@@ -7,6 +8,10 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT /2
+    player = Player(x, y)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -19,6 +24,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
