20ef677c6940e79266195193247428d18c302e4a
DadBytes <bpmckim@icloud.com>
2025-12-16 16:33:22 -0500

    Initial attempt to turn sprite

--- main.py
+++ main.py
@@ -22,11 +22,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+        
+
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
